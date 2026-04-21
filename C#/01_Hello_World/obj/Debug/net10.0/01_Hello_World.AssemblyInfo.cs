ac3fff5af8d111117927f071898f01d6310b3490
BaiMing626AH <baiming020908@163.com>
2026-04-21 10:16:43 +0800

    26.04.21

--- C#/01_Hello_World/obj/Debug/net10.0/01_Hello_World.AssemblyInfo.cs
+++ C#/01_Hello_World/obj/Debug/net10.0/01_Hello_World.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_Hello_World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44097bf2a477856084ac593a1e607d3129dd6df")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_Hello_World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_Hello_World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
